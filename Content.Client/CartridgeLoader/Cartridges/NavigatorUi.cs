1210acd5a803e7cb025a52a56641b9af977a5c9e
Vigers Ray <vigersray@gmail.com>
2025-08-24 07:01:10 +0300

    чиним говно

--- Content.Client/CartridgeLoader/Cartridges/NavigatorUi.cs
+++ Content.Client/CartridgeLoader/Cartridges/NavigatorUi.cs
@@ -1,6 +1,5 @@
 using Content.Client.UserInterface.Fragments;
 using Content.Shared.CartridgeLoader.Cartridges;
-using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
 
 namespace Content.Client.CartridgeLoader.Cartridges;
@@ -8,12 +7,7 @@ namespace Content.Client.CartridgeLoader.Cartridges;
 public sealed partial class NavigatorUi : UIFragment
 {
     private NavigatorUiFragment? _fragment;
-    private IEntityManager _entManager;
-
-    public NavigatorUi()
-    {
-        _entManager = IoCManager.Resolve<IEntityManager>();
-    }
+    private IEntityManager? _entManager;
 
     public override Control GetUIFragmentRoot()
     {
@@ -22,13 +16,14 @@ public sealed partial class NavigatorUi : UIFragment
 
     public override void Setup(BoundUserInterface userInterface, EntityUid? fragmentOwner)
     {
+        _entManager = IoCManager.Resolve<IEntityManager>();
         _fragment = new NavigatorUiFragment();
         _fragment.Setup(fragmentOwner);
     }
 
     public override void UpdateState(BoundUserInterfaceState state)
     {
-        if (state is not NavigatorUiState navigatorState)
+        if (state is not NavigatorUiState navigatorState || _entManager == null)
             return;
 
         var mapUid = _entManager.GetEntity(navigatorState.MapUid);
